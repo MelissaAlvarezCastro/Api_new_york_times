a71353d5ada7af069cd13e5d8afe2835e419cc22
jhonjames0510 <jhonjames0510@gmail.com>
2020-11-26 14:49:17 -0500

    ubuntu key

--- paquetes.sh
+++ paquetes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt -y update
 sudo apt-get -y install python3-pip
+sudo apt -y install python3-venv
+cd /home/ubuntu/Api_new_york_times
+python3 -m venv venv
+source venv/bin/activate
 pip3 install requests
 pip3 install boto3
