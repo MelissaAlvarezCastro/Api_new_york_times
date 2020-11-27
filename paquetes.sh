54ecc95408a060a6131946ead8756966d43d4e86
jhonjames0510 <jhonjames0510@gmail.com>
2020-11-27 17:47:50 -0500

    sh

--- paquetes.sh
+++ paquetes.sh
@@ -1,7 +1,8 @@
 sudo apt -y update
-cd /home/ubuntu/Api_new_york_times
+cd /home/ubuntu
 mkdir consultas
 chmod 700 consultas
+cd /home/ubuntu/Api_new_york_times
 sudo apt -y install python3-venv
 python3 -m venv venv
 source venv/bin/activate
