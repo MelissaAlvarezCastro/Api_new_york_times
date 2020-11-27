473bb7ec205f777bd0033365cf213d43d1d2fd47
jhonjames0510 <jhonjames0510@gmail.com>
2020-11-27 16:42:39 -0500

    sh terminado

--- paquetes.sh
+++ paquetes.sh
@@ -1,6 +1,8 @@
 sudo apt -y update
-sudo apt -y install python3-venv
+cd /home/ubuntu
+git clone https://github.com/MelissaAlvarezCastro/Api_new_york_times.git
 cd /home/ubuntu/Api_new_york_times
+sudo apt -y install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install requests
