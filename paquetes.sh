7976e3f6d9249a17e815a92861b3969f617fa03c
jhonjames0510 <jhonjames0510@gmail.com>
2020-11-27 17:14:38 -0500

    bash corregido otra vez

--- paquetes.sh
+++ paquetes.sh
@@ -1,6 +1,4 @@
 sudo apt -y update
-cd /home/ubuntu
-git clone https://github.com/MelissaAlvarezCastro/Api_new_york_times.git
 cd /home/ubuntu/Api_new_york_times
 sudo apt -y install python3-venv
 python3 -m venv venv
